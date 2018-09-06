e1ad0f21df158e733f9284faf66d7b54be593580
Roman.Yakivchyk <roman.yakivchyk@gmail.com>
2018-09-06 23:05:40 +0300

    add validation messages, other minor changes

--- src/main/java/com/demo/hotel_management/dto/VacationDto.java
+++ src/main/java/com/demo/hotel_management/dto/VacationDto.java
@@ -8,6 +8,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.Date;
@@ -18,25 +19,26 @@ import java.util.Set;
 public class VacationDto {
 
     private Long vacationId;
-    @NotNull
+    @NotNull(message = "{validation.vacation.client.message}")
     private Long clientId;
     private String clientName;
     private String clientOtherInfo;
     private String clientPhoneNumber;
-    @NotNull
+    @NotNull(message = "{validation.vacation.arrival_date.message}")
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate arrivalDate;
     @NotNull
     private Integer arrivalDayPart;
-    @NotNull
+    @NotNull(message = "{validation.vacation.leave_date.message}")
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     private LocalDate leaveDate;
     @NotNull
     private Integer leaveDayPart;
     @NotNull
+    @NotEmpty(message = "{validation.vacation.rooms.message}")
     private Set<Long> roomIds;
     @Min(1)
     @Max(20)
-    @NotNull
+    @NotNull(message = "{validation.vacation.residents.message}")
     private Integer residentsCount;
 }
