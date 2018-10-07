baa49d4264c52dd76440f698c8ea7cb83c5470ac
Roman.Yakivchyk <roman.yakivchyk@gmail.com>
2018-10-07 13:12:38 +0300

    remove H2 and hateoas from dependencies

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO CLIENT (id,name,other_client_info,phone_number,email,inactive)
-VALUES (111,'Віталій','Залужний','0937455233','vitaly@gmail.com','false');
-INSERT INTO CLIENT (id,name,other_client_info,phone_number,email,inactive)
-VALUES (112,'Іван','З Луцька','0324554233','ivan@gmail.com','false');
-INSERT INTO CLIENT (id,name,other_client_info,phone_number,email,inactive)
-VALUES (113,'Ольга','Ввввв','0930455233','oshpk@gmail.com','false');
