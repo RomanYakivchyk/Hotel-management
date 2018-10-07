219f0d228913355933adc7ec52d0874ed0a26fa0
Roman.Yakivchyk <roman.yakivchyk@gmail.com>
2018-10-07 13:58:42 +0300

    change postgresSql config

--- src/main/java/com/demo/hotel_management/HotelManagementApplication.java
+++ src/main/java/com/demo/hotel_management/HotelManagementApplication.java
@@ -9,11 +9,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.Formatter;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
@@ -38,9 +41,35 @@ public class HotelManagementApplication {
 
     @Bean
     @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+    public DataSource postgresDataSource() {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        log.info("Initializing PostgreSQL database: {}", databaseUrl);
+
+        URI dbUri;
+        try {
+            dbUri = new URI(databaseUrl);
+        }
+        catch (URISyntaxException e) {
+            log.error(String.format("Invalid DATABASE_URL: %s", databaseUrl), e);
+            return null;
+        }
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+                + dbUri.getPort() + dbUri.getPath();
+
+        org.apache.tomcat.jdbc.pool.DataSource dataSource
+                = new org.apache.tomcat.jdbc.pool.DataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setTestOnBorrow(true);
+        dataSource.setTestWhileIdle(true);
+        dataSource.setTestOnReturn(true);
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
     }
 
     @Bean
