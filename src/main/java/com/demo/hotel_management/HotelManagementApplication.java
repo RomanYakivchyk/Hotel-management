b2e7e9753ceded51a233989a4a32b126b6443a0a
Roman.Yakivchyk <roman.yakivchyk@gmail.com>
2018-10-23 20:21:03 +0300

    css changes

--- src/main/java/com/demo/hotel_management/HotelManagementApplication.java
+++ src/main/java/com/demo/hotel_management/HotelManagementApplication.java
@@ -83,7 +83,7 @@ public class HotelManagementApplication {
         dataSource.setUrl("jdbc:postgresql://ec2-54-243-147-162.compute-1.amazonaws.com:5432/d4kpiadsgu8ckn");
         dataSource.setUsername("fhcorgnllxvhkc");
         dataSource.setPassword("576936a50df4df8e74335d16c99045df829bfb75a2888933ed3d0807b429dc95");
-//
+
 //        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
 //        dataSource.setUsername("postgres");
 //        dataSource.setPassword("");
